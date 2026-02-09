eab97e9e8c641a167ed8da33a84109e472c10fcc
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-10 00:42:38 +0300

    Level 0 - Hello Ethernaut challenge

---
+++ script/Level0.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../src/Level0.sol";
+import "lib/forge-std/src/Script.sol";
+import "lib/forge-std/src/console.sol";
+
+contract Level0Solution is Script {
+
+    Level0 public level0 = new Level0(0x757d776b500d4524DA673b57Bef7B7E7aC54c67F);
+
+    function run() external {
+        string memory password = level0.password();
+        console.log("Password: ", password);
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        level0.authenticate(password);
+        vm.stopBroadcast();
+    }
+}
