7fbf530f36803fa06993c05c9898d438308daa1b
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-10 00:55:11 +0300

    Level 0 - Hello Ethernaut challenge

--- script/Level0Solution.s.sol
+++ script/Level0Solution.s.sol
@@ -8,7 +8,7 @@ import "lib/forge-std/src/console.sol";
 
 contract Level0Solution is Script {
 
-    Level0 public level0 = new Level0(0x757d776b500d4524DA673b57Bef7B7E7aC54c67F);
+    Level0 public level0 = Level0(0x757d776b500d4524DA673b57Bef7B7E7aC54c67F);
 
     function run() external {
         string memory password = level0.password();
