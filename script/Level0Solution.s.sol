88d8ade89c34e9f511e6a1b27c64b75aea7bbc04
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-11 12:13:34 +0300

    fixed named imports

--- script/Level0Solution.s.sol
+++ script/Level0Solution.s.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "../src/Level0.sol";
-import "lib/forge-std/src/Script.sol";
-import "lib/forge-std/src/console.sol";
+import {Level0} from "../src/Level0.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
+import {console} from "lib/forge-std/src/console.sol";
 
 contract Level0Solution is Script {
     Level0 public level0 = Level0(0x757d776b500d4524DA673b57Bef7B7E7aC54c67F);
