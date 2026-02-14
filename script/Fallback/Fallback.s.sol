2cbfc951f4aad128bd49179a54174ca4cd15f180
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-14 22:56:11 +0300

    fixed solc version setups

--- script/Fallback/Fallback.s.sol
+++ script/Fallback/Fallback.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.13;
+pragma solidity >=0.8.0;
 
 import {Fallback} from "src/Fallback/Fallback.sol";
 import {Script} from "lib/forge-std/src/Script.sol";
